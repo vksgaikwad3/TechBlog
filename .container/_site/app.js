d14485cdf68ffac5f4dc956e80c6b0b709171064
vikas <vikas@infinite-uptime.com>
2020-09-05 12:18:39 +0530

    Updated the footer and customised messages

--- .container/_site/app.js
+++ .container/_site/app.js
@@ -1,1 +1,1 @@
-/* Registering Service Worker */ if('serviceWorker' in navigator) { navigator.serviceWorker.register('/TechBlog/sw.js'); };
+/* Registering Service Worker */ if('serviceWorker' in navigator) { navigator.serviceWorker.register('/TechTalk/sw.js'); };
